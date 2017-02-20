c4a9d71aee67629899d19bb465ebc712c62e2126
Bert Ramakers <bertramakers@me.com>
2017-02-20 18:32:13 +0100

    III-1779: Add controller to search through offers.

---
+++ src/OfferSearchController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace CultuurNet\UDB3\Search\Http;
+
+use CultuurNet\UDB3\Search\JsonDocument\JsonDocumentTransformerInterface;
+use CultuurNet\UDB3\Search\JsonDocument\PassThroughJsonDocumentTransformer;
+use CultuurNet\UDB3\Search\Offer\OfferSearchParameters;
+use CultuurNet\UDB3\Search\Offer\OfferSearchServiceInterface;
+use CultuurNet\UDB3\Search\Region\RegionId;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use ValueObjects\Number\Natural;
+use ValueObjects\StringLiteral\StringLiteral;
+
+class OfferSearchController
+{
+    /**
+     * @var OfferSearchServiceInterface
+     */
+    private $searchService;
+
+    /**
+     * @var StringLiteral
+     */
+    private $regionIndexName;
+
+    /**
+     * @var StringLiteral
+     */
+    private $regionDocumentType;
+
+    /**
+     * @var PagedCollectionFactoryInterface
+     */
+    private $pagedCollectionFactory;
+
+    /**
+     * @param OfferSearchServiceInterface $searchService
+     * @param StringLiteral $regionIndexName
+     * @param StringLiteral $regionDocumentType
+     * @param PagedCollectionFactoryInterface|null $pagedCollectionFactory
+     */
+    public function __construct(
+        OfferSearchServiceInterface $searchService,
+        StringLiteral $regionIndexName,
+        StringLiteral $regionDocumentType,
+        PagedCollectionFactoryInterface $pagedCollectionFactory = null
+    ) {
+        if (is_null($pagedCollectionFactory)) {
+            $pagedCollectionFactory = new PagedCollectionFactory();
+        }
+
+        $this->searchService = $searchService;
+        $this->regionIndexName = $regionIndexName;
+        $this->regionDocumentType = $regionDocumentType;
+        $this->pagedCollectionFactory = $pagedCollectionFactory;
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    public function search(Request $request)
+    {
+        $start = (int) $request->query->get('start', 0);
+        $limit = (int) $request->query->get('limit', 30);
+        $embed = (bool) $request->query->get('embed', false);
+
+        if ($limit == 0) {
+            $limit = 30;
+        }
+
+        $parameters = (new OfferSearchParameters())
+            ->withStart(new Natural($start))
+            ->withLimit(new Natural($limit));
+
+        if (!empty($request->query->get('regionId'))) {
+            $parameters = $parameters->withRegion(
+                new RegionId($request->query->get('regionId')),
+                $this->regionIndexName,
+                $this->regionDocumentType
+            );
+        }
+
+        $resultSet = $this->searchService->search($parameters);
+
+        $pagedCollection = $this->pagedCollectionFactory->fromPagedResultSet(
+            $resultSet,
+            $start,
+            $limit,
+            $embed
+        );
+
+        return (new JsonResponse($pagedCollection, 200, ['Content-Type' => 'application/ld+json']))
+            ->setPublic()
+            ->setClientTtl(60 * 1)
+            ->setTtl(60 * 5);
+    }
+}
---
+++ tests/OfferSearchControllerTest.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace CultuurNet\UDB3\Search\Http;
+
+use CultuurNet\UDB3\ReadModel\JsonDocument;
+use CultuurNet\UDB3\Search\Offer\OfferSearchParameters;
+use CultuurNet\UDB3\Search\Offer\OfferSearchServiceInterface;
+use CultuurNet\UDB3\Search\PagedResultSet;
+use CultuurNet\UDB3\Search\Region\RegionId;
+use Symfony\Component\HttpFoundation\Request;
+use ValueObjects\Number\Natural;
+use ValueObjects\StringLiteral\StringLiteral;
+use ValueObjects\Web\Url;
+
+class OfferSearchControllerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var OfferSearchServiceInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $searchService;
+
+    /**
+     * @var StringLiteral
+     */
+    private $regionIndexName;
+
+    /**
+     * @var StringLiteral
+     */
+    private $regionDocumentType;
+
+    /**
+     * @var OfferSearchController
+     */
+    private $controller;
+
+    public function setUp()
+    {
+        $this->searchService = $this->createMock(OfferSearchServiceInterface::class);
+
+        $this->regionIndexName = new StringLiteral('geoshapes');
+        $this->regionDocumentType = new StringLiteral('region');
+
+        $this->controller = new OfferSearchController(
+            $this->searchService,
+            $this->regionIndexName,
+            $this->regionDocumentType
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_a_paged_collection_of_search_results_based_on_request_query_parameters()
+    {
+        $request = new Request(
+            [
+                'start' => 30,
+                'limit' => 10,
+                'regionId' => 'gem-leuven',
+            ]
+        );
+
+        $expectedSearchParameters = (new OfferSearchParameters())
+            ->withRegion(
+                new RegionId('gem-leuven'),
+                $this->regionIndexName,
+                $this->regionDocumentType
+            )
+            ->withStart(new Natural(30))
+            ->withLimit(new Natural(10));
+
+        $expectedResultSet = new PagedResultSet(
+            new Natural(32),
+            new Natural(10),
+            [
+                new JsonDocument('3f2ba18c-59a9-4f65-a242-462ad467c72b', '{"@id": "events/1"}'),
+                new JsonDocument('39d06346-b762-4ccd-8b3a-142a8f6abbbe', '{"@id": "places/2"}'),
+            ]
+        );
+
+        $this->searchService->expects($this->once())
+            ->method('search')
+            ->with($expectedSearchParameters)
+            ->willReturn($expectedResultSet);
+
+        $expectedJsonResponse = json_encode(
+            [
+                '@context' => 'http://www.w3.org/ns/hydra/context.jsonld',
+                '@type' => 'PagedCollection',
+                'itemsPerPage' => 10,
+                'totalItems' => 32,
+                'member' => [
+                    ['@id' => 'events/1'],
+                    ['@id' => 'places/2'],
+                ],
+            ]
+        );
+
+        $actualJsonResponse = $this->controller->search($request)
+            ->getContent();
+
+        $this->assertEquals($expectedJsonResponse, $actualJsonResponse);
+    }
+
+    /**
+     * @test
+     */
+    public function it_uses_the_default_limit_of_30_if_a_limit_of_0_is_given()
+    {
+        $request = new Request(
+            [
+                'start' => 0,
+                'limit' => 0,
+            ]
+        );
+
+        $expectedSearchParameters = (new OfferSearchParameters())
+            ->withStart(new Natural(0))
+            ->withLimit(new Natural(30));
+
+        $expectedResultSet = new PagedResultSet(new Natural(30), new Natural(0), []);
+
+        $this->searchService->expects($this->once())
+            ->method('search')
+            ->with($expectedSearchParameters)
+            ->willReturn($expectedResultSet);
+
+        $this->controller->search($request);
+    }
+}
