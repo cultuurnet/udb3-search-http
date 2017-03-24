bc5dfedb45cd5f9d2c5407f6102ed0843af90eed
Bert Ramakers <bertramakers@me.com>
2017-03-24 12:20:49 +0100

    III-1999: Add geo distance parameters to offer search endpoint

--- src/OfferSearchController.php
+++ src/OfferSearchController.php
@@ -2,9 +2,12 @@
 
 namespace CultuurNet\UDB3\Search\Http;
 
+use CultuurNet\Geocoding\Coordinate\Coordinates;
 use CultuurNet\UDB3\Label\ValueObjects\LabelName;
 use CultuurNet\UDB3\Language;
 use CultuurNet\UDB3\PriceInfo\Price;
+use CultuurNet\UDB3\Search\DistanceFactoryInterface;
+use CultuurNet\UDB3\Search\GeoDistanceParameters;
 use CultuurNet\UDB3\Search\Offer\OfferSearchParameters;
 use CultuurNet\UDB3\Search\Offer\OfferSearchServiceInterface;
 use CultuurNet\UDB3\Search\QueryStringFactoryInterface;
@@ -33,6 +36,16 @@ class OfferSearchController
     private $regionDocumentType;
 
     /**
+     * @var QueryStringFactoryInterface
+     */
+    private $queryStringFactory;
+
+    /**
+     * @var DistanceFactoryInterface
+     */
+    private $distanceFactory;
+
+    /**
      * @var PagedCollectionFactoryInterface
      */
     private $pagedCollectionFactory;
@@ -42,6 +55,7 @@ class OfferSearchController
      * @param StringLiteral $regionIndexName
      * @param StringLiteral $regionDocumentType
      * @param QueryStringFactoryInterface $queryStringFactory
+     * @param DistanceFactoryInterface $distanceFactory
      * @param PagedCollectionFactoryInterface|null $pagedCollectionFactory
      */
     public function __construct(
@@ -49,6 +63,7 @@ class OfferSearchController
         StringLiteral $regionIndexName,
         StringLiteral $regionDocumentType,
         QueryStringFactoryInterface $queryStringFactory,
+        DistanceFactoryInterface $distanceFactory,
         PagedCollectionFactoryInterface $pagedCollectionFactory = null
     ) {
         if (is_null($pagedCollectionFactory)) {
@@ -59,6 +74,7 @@ class OfferSearchController
         $this->regionIndexName = $regionIndexName;
         $this->regionDocumentType = $regionDocumentType;
         $this->queryStringFactory = $queryStringFactory;
+        $this->distanceFactory = $distanceFactory;
         $this->pagedCollectionFactory = $pagedCollectionFactory;
     }
 
@@ -104,6 +120,23 @@ class OfferSearchController
                 $this->regionDocumentType
             );
         }
+
+        $coordinates = $request->query->get('coordinates', false);
+        $distance = $request->query->get('distance', false);
+
+        if ($coordinates && !$distance) {
+            throw new \InvalidArgumentException('Required "distance" parameter missing when searching by coordinates.');
+        } elseif ($distance && !$coordinates) {
+            throw new \InvalidArgumentException('Required "coordinates" parameter missing when searching by distance.');
+        } elseif ($coordinates && $distance) {
+            $parameters = $parameters->withGeoDistanceParameters(
+                new GeoDistanceParameters(
+                    Coordinates::fromLatLonString($coordinates),
+                    $this->distanceFactory->fromString($distance)
+                )
+            );
+        }
+
 
         // Do strict comparison to make sure 0 gets included.
         if ($request->query->get('minAge', false) !== false) {
