270bed5420c16055f7fdf2765b39dcfcbb62e839
Bert Ramakers <bertramakers@me.com>
2017-05-17 15:05:13 +0200

    III-2072: Add text parameter to offer search endpoint

--- tests/OfferSearchControllerTest.php
+++ tests/OfferSearchControllerTest.php
@@ -106,7 +106,8 @@ class OfferSearchControllerTest extends \PHPUnit_Framework_TestCase
             [
                 'start' => 30,
                 'limit' => 10,
-                'q' => 'dag van de fiets',
+                'q' => 'dag van de fiets AND labels:foo',
+                'text' => '(foo OR bar) AND baz',
                 'id' => '42926044-09f4-4bd5-bc35-427b2fc1a525',
                 'locationId' => '652ab95e-fdff-41ce-8894-1b29dce0d230',
                 'organizerId' => '392168d7-57c9-4488-8e2e-d492c843054b',
@@ -151,7 +152,10 @@ class OfferSearchControllerTest extends \PHPUnit_Framework_TestCase
 
         $expectedSearchParameters = (new OfferSearchParameters())
             ->withQueryString(
-                new MockQueryString('dag van de fiets')
+                new MockQueryString('dag van de fiets AND labels:foo')
+            )
+            ->withText(
+                new StringLiteral('(foo OR bar) AND baz')
             )
             ->withCdbid(
                 new Cdbid('42926044-09f4-4bd5-bc35-427b2fc1a525')
