4f73d6652b858f12e44bae7f23c0801aa8968f06
Bert Ramakers <bertramakers@me.com>
2017-05-22 15:51:06 +0200

    III-2068: Add request authenticator based on api keys

---
+++ src/Authentication/ApiKeyRequestAuthenticator.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace CultuurNet\UDB3\Search\Http\Authentication;
+
+use CultuurNet\UDB3\Search\Authentication\ApiKeyAuthenticationException;
+use CultuurNet\UDB3\Search\Authentication\ApiKeyAuthenticatorInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class ApiKeyRequestAuthenticator implements RequestAuthenticatorInterface
+{
+    /**
+     * @var ApiKeyReaderInterface
+     */
+    private $apiKeyReader;
+
+    /**
+     * @var ApiKeyAuthenticatorInterface
+     */
+    private $apiKeyAuthenticator;
+
+    /**
+     * @param ApiKeyReaderInterface $apiKeyReader
+     * @param ApiKeyAuthenticatorInterface $apiKeyAuthenticator
+     */
+    public function __construct(
+        ApiKeyReaderInterface $apiKeyReader,
+        ApiKeyAuthenticatorInterface $apiKeyAuthenticator
+    ) {
+        $this->apiKeyReader = $apiKeyReader;
+        $this->apiKeyAuthenticator = $apiKeyAuthenticator;
+    }
+
+    /**
+     * @param Request $request
+     * @throws RequestAuthenticationException
+     */
+    public function authenticate(Request $request)
+    {
+        $apiKey = $this->apiKeyReader->read($request);
+
+        if (is_null($apiKey)) {
+            throw new RequestAuthenticationException('No API key provided.');
+        }
+
+        try {
+            $this->apiKeyAuthenticator->authenticate($apiKey);
+        } catch (ApiKeyAuthenticationException $e) {
+            throw new RequestAuthenticationException("Invalid API key provided ({$apiKey->toNative()}).");
+        }
+    }
+}
---
+++ tests/Authentication/ApiKeyRequestAuthenticatorTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace CultuurNet\UDB3\Search\Http\Authentication;
+
+use CultuurNet\UDB3\Search\Authentication\ApiKey;
+use CultuurNet\UDB3\Search\Authentication\ApiKeyAuthenticationException;
+use CultuurNet\UDB3\Search\Authentication\ApiKeyAuthenticatorInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class ApiKeyRequestAuthenticatorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var QueryParameterApiKeyReader
+     */
+    private $apiKeyReader;
+
+    /**
+     * @var ApiKeyAuthenticatorInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $apiKeyAuthenticator;
+
+    /**
+     * @var ApiKeyRequestAuthenticator
+     */
+    private $requestAuthenticator;
+
+    public function setUp()
+    {
+        $this->apiKeyReader = new QueryParameterApiKeyReader('apiKey');
+        $this->apiKeyAuthenticator = $this->createMock(ApiKeyAuthenticatorInterface::class);
+
+        $this->requestAuthenticator = new ApiKeyRequestAuthenticator(
+            $this->apiKeyReader,
+            $this->apiKeyAuthenticator
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_throw_an_exception_if_no_api_key_can_be_found_in_the_request()
+    {
+        $request = new Request();
+
+        $this->expectException(RequestAuthenticationException::class);
+        $this->expectExceptionMessage('No API key provided.');
+
+        $this->requestAuthenticator->authenticate($request);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_throw_an_exception_if_the_provided_api_key_is_invalid()
+    {
+        $request = new Request(['apiKey' => '0649b422-98c2-4ea0-a2c6-65bf935d11d5']);
+
+        $this->apiKeyAuthenticator->expects($this->once())
+            ->method('authenticate')
+            ->with(new ApiKey('0649b422-98c2-4ea0-a2c6-65bf935d11d5'))
+            ->willThrowException(new ApiKeyAuthenticationException('API key is invalid!!!11!'));
+
+        $this->expectException(RequestAuthenticationException::class);
+        $this->expectExceptionMessage("Invalid API key provided (0649b422-98c2-4ea0-a2c6-65bf935d11d5).");
+
+        $this->requestAuthenticator->authenticate($request);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_not_throw_an_exception_if_the_api_key_does_not_raise_an_authentication_exception()
+    {
+        $request = new Request(['apiKey' => '0649b422-98c2-4ea0-a2c6-65bf935d11d5']);
+
+        $this->apiKeyAuthenticator->expects($this->once())
+            ->method('authenticate')
+            ->with(new ApiKey('0649b422-98c2-4ea0-a2c6-65bf935d11d5'));
+
+        $this->requestAuthenticator->authenticate($request);
+    }
+}
