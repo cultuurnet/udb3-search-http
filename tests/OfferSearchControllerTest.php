2665aa9f760e293716fc316768d96290ffe2a951
Luc Wollants <luc.wollants@apptite.be>
2017-05-29 13:08:23 +0200

    III-2099 Remove obsolete namespace SortBy.

--- tests/OfferSearchControllerTest.php
+++ tests/OfferSearchControllerTest.php
@@ -21,7 +21,6 @@ use CultuurNet\UDB3\Search\Offer\Cdbid;
 use CultuurNet\UDB3\Search\Offer\FacetName;
 use CultuurNet\UDB3\Search\Offer\OfferQueryBuilderInterface;
 use CultuurNet\UDB3\Search\Offer\OfferSearchServiceInterface;
-use CultuurNet\UDB3\Search\Offer\SortBy;
 use CultuurNet\UDB3\Search\Offer\WorkflowStatus;
 use CultuurNet\UDB3\Search\Offer\TermId;
 use CultuurNet\UDB3\Search\Offer\TermLabel;
